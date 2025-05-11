8b53afe71c7fb76d190d09b9d12b1dd44686b1e2
Hyunjiiing <h.jilluminate@gmail.com>
2025-05-11 22:35:50 +0900

    fix: request, response에 대한 DTO 분리

--- AI/main.py
+++ AI/main.py
@@ -4,15 +4,23 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class VoiceResponse(BaseModel):
+    voice: str
+    
+    
 @app.post("/voice")
-async def chat_voice(audio: UploadFile = File(..., description="WAV/MP3 file")):
+async def chat_voice(audio: UploadFile = File(..., description="WAV/MP3 file")) -> VoiceResponse:
     return audio.filename
 
 
 class ChattingRequest(BaseModel):
     chats: str
     
+    
+class SummaryDiaryResponse(BaseModel):
+    diary: str
+
 
 @app.post("/summary")
-def create_summary_diary(request: ChattingRequest) -> str:
+def create_summary_diary(request: ChattingRequest) -> SummaryDiaryResponse:
     return request.chats
