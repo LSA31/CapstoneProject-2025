8bdcf5278ea2f57ff803d934a3ada73c3880a6e4
Hyunjiiing <h.jilluminate@gmail.com>
2025-05-11 22:27:24 +0900

    feat: 필요한 API 컨트롤러 구성

--- AI/main.py
+++ AI/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return "World"
+
+@app.post("/voice")
+async def chat_voice(audio: UploadFile = File(..., description="WAV/MP3 file")):
+    return audio.filename
+
+
+class ChattingRequest(BaseModel):
+    chats: str
+    
+
+@app.post("/summary")
+def create_summary_diary(request: ChattingRequest) -> str:
+    return request.chats
